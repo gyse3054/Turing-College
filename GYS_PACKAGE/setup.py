42943cd67eed31701b31cdb1a2e589fd33324122
gyse3054 <gytisse@gmail.com>
2023-10-12 19:50:49 +0300

    new file:   GYS_PACKAGE.zip 	new file:   GYS_PACKAGE/setup.py 	modified:   test2.py

---
+++ GYS_PACKAGE/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GYS_PACKAGE",
+    version="1.0.0",
+    description="My calculator",
+    author="Gytis Semenas",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
--- test2.py
+++ test2.py
@@ -1,4 +1,1 @@
-import sys
-
-sys.path.append(r'C:\Users\gytis\Desktop\Turing College\GYS_PACKAGE')
-print(sys.path)
+from google.colab import drive
